a88e6d920623dada417f452089e71df3dd7d76b9
thevrus <vadimrusin94@gmail.com>
2020-10-19 16:35:30 +0200

    Added slots to Panel component

--- src/components/panel/Panel.vue
+++ src/components/panel/Panel.vue
@@ -1,11 +1,18 @@
 <template>
-	<div class="panel">
-		<Tabs />
-		<div class="panel-body">
-			<Comments />
-			<Exercises />
-			<Materials />
-		</div>
+	<div>
+		<Tabs :tabs="tabs" :initialTab="tabs[0]">
+			<template slot="questions">
+				<Comments />
+			</template>
+
+			<template slot="exercises">
+				<Exercises />
+			</template>
+
+			<template slot="materials">
+				<Materials />
+			</template>
+		</Tabs>
 	</div>
 </template>
 
@@ -16,25 +23,21 @@ import Exercises from '@/components/panel/exercises/Exercises'
 import Materials from '@/components/panel/materials/Materials'
 
 export default {
+	name: 'Panel',
 	components: {
 		Tabs,
 		Comments,
 		Exercises,
 		Materials,
 	},
+	data() {
+		return {
+			tabs: [
+				{ name: 'questions', value: 'Вопросы и ответы' },
+				{ name: 'exercises', value: 'Задания' },
+				{ name: 'materials', value: 'Материалы' },
+			],
+		}
+	},
 }
 </script>
-
-<style lang="postcss" scoped>
-.panel {
-	background-color: var(--panel-bg);
-	padding-bottom: 2rem;
-	border-radius: 10px;
-	border: 1px solid var(--panel-border);
-	box-shadow: 0px 2px 6px 0px #000000 20%;
-
-	&-body {
-		padding: 0 4rem;
-	}
-}
-</style>
--- src/components/panel/Tabs.vue
+++ src/components/panel/Tabs.vue
@@ -1,17 +1,55 @@
-<template functional>
-	<div class="tabs">
-		<div class="tab tab--active">Вопросы и ответы</div>
-		<div class="tab">Задания</div>
-		<div class="tab">Материалы</div>
+<template>
+	<div class="panel">
+		<ul class="tabs">
+			<li
+				v-for="(tab, index) in tabs"
+				:key="index"
+				class="tab"
+				:class="{ active: activeTab.name == tab.name }"
+				@click="switchTab(tab)"
+			>
+				{{ tab.value }}
+			</li>
+		</ul>
+
+		<div class="panel-body">
+			<transition name="fade" mode="out-in">
+				<slot :name="tabPanelSlotName"></slot>
+			</transition>
+		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		initialTab: Object,
+		tabs: Array,
+	},
+	data() {
+		return {
+			activeTab: this.initialTab,
+		}
+	},
+	computed: {
+		tabPanelSlotName() {
+			return this.activeTab.name
+		},
+	},
+	methods: {
+		switchTab(tabName) {
+			this.activeTab = tabName
+		},
+	},
+}
+</script>
 
 <style lang="postcss" scoped>
 .tabs {
 	display: flex;
 	justify-content: space-between;
 	color: #6c6c6c;
-	border-bottom: 1px solid #3a3a3a;
+	border-bottom: 0.5px solid #3a3a3a;
 	margin-bottom: 2.5rem;
 
 	.tab {
@@ -21,27 +59,39 @@
 		width: 100%;
 		transition: 0.3s color;
 
-		&--active {
-			color: #fff;
-			position: relative;
-
-			&::after {
-				content: '';
-				display: block;
-				position: absolute;
-				bottom: -1px;
-				border-top-left-radius: 4px;
-				border-top-right-radius: 4px;
-				left: 45%;
-				width: 40px;
-				height: 3px;
-				background-color: #733bea;
-			}
-		}
-
 		&:hover {
 			color: #fff;
 		}
+	}
+
+	.active {
+		color: #fff;
+		position: relative;
+
+		&::after {
+			content: '';
+			display: block;
+			position: absolute;
+			bottom: -1px;
+			border-top-left-radius: 4px;
+			border-top-right-radius: 4px;
+			left: 45%;
+			width: 40px;
+			height: 3px;
+			background-color: var(--secondary);
+		}
+	}
+}
+
+.panel {
+	background-color: var(--panel-bg);
+	padding-bottom: 2rem;
+	border-radius: 10px;
+	border: 1px solid var(--panel-border);
+	box-shadow: 0px 2px 6px 0px #000000 20%;
+
+	&-body {
+		padding: 0 4rem;
 	}
 }
 </style>
