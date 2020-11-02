885f9b1cc6e1c42fdd929282744e1d06c4c69ffa
thevrus <vadimrusin94@gmail.com>
2020-11-02 14:09:56 +0100

    Crisp chat replaced with Telegram

---
+++ src/components/Chat.vue
@@ -0,0 +1,31 @@
+<template functional>
+	<a
+		href="https://www.t.me/designeroq"
+		target="_blank"
+		class="chat"
+		title="Задать вопрос"
+	>
+	</a>
+</template>
+
+<style lang="postcss" scoped>
+.chat {
+	width: 54px !important;
+	height: 54px !important;
+	border-radius: 54px !important;
+	background-color: #252525;
+	position: fixed;
+	z-index: 10;
+	bottom: 24px;
+	right: 24px;
+	cursor: pointer;
+	transition: opacity 0.3s;
+	background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIyOCIgZmlsbD0ibm9uZSI+PGRlZnMvPjxwYXRoIGZpbGw9IiNmZmYiIGQ9Ik0zMiAxbC01IDI1cy0xIDItMyAxbC0xMi05LTQtMi03LTItMS0xIDEtMkwyOSAwczMtMSAzIDF6Ii8+PHBhdGggZmlsbD0iI0QyRTVGMSIgZD0iTTEyIDI2bC0xLTEtMy05TDI1IDZoMUwxMiAxOCIvPjxwYXRoIGZpbGw9IiNCNUNGRTQiIGQ9Ik0xNyAyMmwtNSA0di04Ii8+PC9zdmc+');
+	background-repeat: no-repeat;
+	background-position: 40% center;
+
+	&:hover {
+		opacity: 0.8;
+	}
+}
+</style>
