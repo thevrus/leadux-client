f8f5a6a430db6b904374e2a50fadf087cc613f28
thevrus <vadimrusin94@gmail.com>
2020-12-12 15:25:01 +0100

    1.2.1
    * Hotjar added, production checks added
    * Production check added to crisp chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,12 +3,14 @@
 <script>
 export default {
 	mounted() {
-		window.$crisp = []
-		window.CRISP_WEBSITE_ID = '9e1dfe79-2774-4204-b877-5197d83762bd'
-		const script = document.createElement('script')
-		script.setAttribute('src', 'https://client.crisp.chat/l.js')
-		script.async = 1
-		document.head.appendChild(script)
+		if (process.env.VUE_APP_PROD === true) {
+			window.$crisp = []
+			window.CRISP_WEBSITE_ID = '9e1dfe79-2774-4204-b877-5197d83762bd'
+			const script = document.createElement('script')
+			script.setAttribute('src', 'https://client.crisp.chat/l.js')
+			script.async = 1
+			document.head.appendChild(script)
+		}
 	},
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import VueMeta from 'vue-meta'
 import VueShowdown from 'vue-showdown'
 import VueGtag from 'vue-gtag'
-import router from './router'
+import Hotjar from 'vue-hotjar'
 import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueMeta)
@@ -19,9 +20,14 @@ Vue.use(
 		config: { id: 'G-JLFP5VWW29' },
 		appName: 'Leadux',
 		pageTrackerScreenviewEnabled: true,
+		enabled: process.env.VUE_APP_PROD === true,
 	},
 	router
 )
+Vue.use(Hotjar, {
+	id: '2153201',
+	isProduction: process.env.VUE_APP_PROD === true,
+})
 
 new Vue({
 	router,
