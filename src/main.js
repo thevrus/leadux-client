b53bc25f84629e7603fd7423c217a38a891a584f
thevrus <vadimrusin94@gmail.com>
2020-12-13 15:30:35 +0100

    1.2.2
    * environment variable check fix
    * Typo fix

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import VueMeta from 'vue-meta'
 import VueShowdown from 'vue-showdown'
 import VueGtag from 'vue-gtag'
 import Hotjar from 'vue-hotjar'
-import store from './store'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueMeta)
@@ -20,13 +20,13 @@ Vue.use(
 		config: { id: 'G-JLFP5VWW29' },
 		appName: 'Leadux',
 		pageTrackerScreenviewEnabled: true,
-		enabled: process.env.VUE_APP_PROD === true,
+		enabled: process.env.VUE_APP_PROD === 'true',
 	},
 	router
 )
 Vue.use(Hotjar, {
 	id: '2153201',
-	isProduction: process.env.VUE_APP_PROD === true,
+	isProduction: process.env.VUE_APP_PROD === 'true',
 })
 
 new Vue({
